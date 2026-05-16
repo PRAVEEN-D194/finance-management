5ec8ceaeb811a28193f0bde304becc12c651d20c
PRAVEEN-D194 <praveend12321@gmail.com>
2026-05-16 12:33:40 +0530

    this for create router and components

---
+++ backend/modules/customerSchema.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  //   phone: {
+  //     type: String,
+  //     required: true,
+  //   },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+
+  interestPercent: {
+    type: Number,
+    required: true,
+  },
+
+  remainingAmount: {
+    type: Number,
+  },
+  //   status: {
+  //     type: String,
+  //     default: "Pending",
+  //   },
+
+  createdAt: {
+    type: Date,
+    default: () => new Date().toISOString().split("T")[0],
+  },
+
+});
+
+customerSchema.pre("save", function(next){
+  this.remainingAmount = this.totalAmount + ((this.totalAmount * this.interestPercent) / 100);
+})    
+
+module.exports = mongoose.model("customerSchema", customerSchema);
