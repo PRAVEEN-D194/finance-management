08454b07d81e83e048bbe48508cfb84b3f89f47f
PRAVEEN-D194 <praveend12321@gmail.com>
2026-05-18 10:01:30 +0530

    sample for push

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -1,11 +1,12 @@
 //this for customer router
 const express = require("express");
 const router = express.Router();
-const {getallcustomer, postcustomer, deletecustomer, getsinglecustomer} = require("../components/customer");
+const {getallcustomer, postcustomer, deletecustomer, getsinglecustomer, updatecustomer} = require("../components/customer");
 
 router.get('/customer', getallcustomer);
 router.get('/customer/:id', getsinglecustomer);
 router.post('/customer', postcustomer);
+router.put('/customer/:id', updatecustomer);
 router.delete('/customer/:id', deletecustomer);
 
 module.exports = router;
